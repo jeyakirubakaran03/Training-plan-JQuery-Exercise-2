33a52ece1e44cbd72543d05bb9a95f69ae8620e3
jeyakirubakaran03 <jeyakirubakaran03@gmail.com>
2025-08-25 10:59:34 +0530

    modified

--- script2.js
+++ script2.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  
-    $(".mypage-header-nav").addClass("header-height-decrease");
+
+    $("#mypage-header-nav").addClass("header-shrink");
     
     // $(".mypage-header-nav").hover(function(){
     //     $(this).stop(true).animate({height: "100px"});   
@@ -9,27 +9,22 @@ $(document).ready(function(){
     //     $(this).stop(true).animate({height: "10px"});
     // })
 
-  $(".mypage-header-nav").hover(function(){
-    $(this).removeClass("shrink-in-transition header-height-decrease").addClass("shrink-out-transition header-height-increase");
-  },
-  function(){
-    $(this).removeClass("shrink-out-transition header-height-increase").addClass("shrink-in-transition header-height-decrease");
+  $("#mypage-header-nav").hover(function(){
+    $(this).toggleClass("header-shrink header-expand");
   })
 
   /* ***********slideDown() implementation for footer********* */
 
-  $(".mypage-footer").addClass("adding-footer-border");
-  $(".mypage-footer-footer").addClass("mypage-footer-footer-hide");
+  $(".mypage-footer-content").hide();
 
-  $(".mypage-footer").on("mouseenter", function () {
-    $(".mypage-footer-footer").stop(true, true).slideDown(10000, function () {
+  $("#mypage-footer").on("mouseenter", function () {
+    $(".mypage-footer-content").stop(true, true).slideDown(10000, function () {
       alert("Animation Complete!")
     })
   })
-  
-  $(".mypage-footer").on("mouseleave", function(){
-    $(".mypage-footer-footer").stop(true).slideUp(1000);
+
+  $("#mypage-footer").on("mouseleave", function(){
+    $(".mypage-footer-content").stop(true).slideUp(1000);
   })
-
-
+  
 })
